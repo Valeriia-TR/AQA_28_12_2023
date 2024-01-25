1fb68c1d5ed0ea2f516cfc92a9598123a5c10166
Valeriia Bakanova <valeriia.b@gowombat.team>
2024-01-25 23:40:50 +0200

    update

---
+++ AQA_28_12_2023/HomeWorks/homework_to_lesson_7.py
@@ -0,0 +1,25 @@
+# Exercise 1
+
+list_1 = [1, 2, 5, 6, 7, 9, 10]
+list_2 = [8, 9, 10, 4]
+match = list(filter(lambda x: x in list_1, list_2))
+print(match)
+
+# Exercise 2
+
+is_digit = lambda x: x.isdigit()
+value = "82918"
+result = "The value is digit" if is_digit(value) else "The value is not a digit"
+print(result)
+
+# Exercise 3
+
+list_3 = [1, 2, 3, 4, 5, 6, 7, 8]
+list_4 = [1, 2, 3, 4, 5]
+
+max_len = lambda x,y: x if len(x) > len(y) else y
+min_len = lambda x,y: x if len(x) < len(y) else y
+
+result_2 = f"The {max_len(list_3, list_4)} is longer than the {min_len(list_3, list_4)}"
+
+print(result_2)
